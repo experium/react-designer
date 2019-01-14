6ae2b4ea00fe8ce9458dcd63595e927ef3c92762
Nastasia <muaaaaaaa@mail.ru>
2019-01-14 10:00:49 +0300

    add styles for checkbox

--- designer/src/components/settings/Checkbox.js
+++ designer/src/components/settings/Checkbox.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Container = styled.label`
+    cursor: pointer;
+    width: 14px;
+    position: relative;
+    input {
+        position: absolute;
+        opacity: 0;
+        height: 0;
+        width: 0;
+    }
+`;
+
+const Checkmark = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 14px;
+    width: 14px;
+    background-color: rgb(225,225,225);
+    border-radius: 3px;
+    color: darkslategray;
+    display: inline-block;
+    font-size: 10px;
+    padding-left: 2px;
+    padding-top: 1px;
+`;
 
 import CurrentContext from '../context/CurrentContext';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 export default class Checkbox extends Component {
     static propTypes = {
@@ -13,10 +43,15 @@ export default class Checkbox extends Component {
         const value = !!(current || settings)[prop];
         const fn = current ? onChangeProp : onChangeSettingsProp;
 
-        return <input
-            type='checkbox'
-            checked={value}
-            onChange={e => fn(prop, e.target.checked)} />;
+        return <Container>
+            <input
+                type='checkbox'
+                checked={value}
+                onChange={e => fn(prop, e.target.checked)} />
+            <Checkmark>
+                { value && <FontAwesomeIcon icon={faCheck} /> }
+            </Checkmark>
+        </Container>;
     }
 
     render() {
